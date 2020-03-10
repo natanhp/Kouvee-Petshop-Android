dce135722cbca4837914764b0f0f0a97271c38d0
Natan Hari Pamungkas <natanhp@protonmail.com>
2020-03-10 14:49:53 +0700

    Add compiler for room db
    Signed-off-by: Natan Hari Pamungkas <natanhp@protonmail.com>

--- app/src/main/java/com/p3lj2/koveepetshop/dao/EmployeeDAO.java
+++ app/src/main/java/com/p3lj2/koveepetshop/dao/EmployeeDAO.java
@@ -1,9 +1,11 @@
 package com.p3lj2.koveepetshop.dao;
 
+import androidx.room.Dao;
 import androidx.room.Insert;
 
 import com.p3lj2.koveepetshop.model.EmployeeDataModel;
 
+@Dao
 public interface EmployeeDAO {
     @Insert
     void insert(EmployeeDataModel employeeDataModel);
