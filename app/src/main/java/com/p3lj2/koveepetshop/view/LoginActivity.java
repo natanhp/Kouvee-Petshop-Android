d3687121920effe57c480a0b22af16ab0a2a4253
Natan Hari Pamungkas <natanhp@protonmail.com>
2020-03-11 18:26:45 +0700

    Add a progress bar
    Signed-off-by: Natan Hari Pamungkas <natanhp@protonmail.com>

--- app/src/main/java/com/p3lj2/koveepetshop/view/LoginActivity.java
+++ app/src/main/java/com/p3lj2/koveepetshop/view/LoginActivity.java
@@ -1,17 +1,21 @@
 package com.p3lj2.koveepetshop.view;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProvider;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.p3lj2.koveepetshop.R;
 import com.p3lj2.koveepetshop.viewmodel.EmployeeViewModel;
 
 import java.util.List;
 
+import butterknife.BindView;
 import butterknife.BindViews;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -21,6 +25,9 @@ public class LoginActivity extends AppCompatActivity {
 
     @BindViews({R.id.edt_username, R.id.edt_password})
     List<EditText> loginForms;
+
+    @BindView(R.id.progress_bar)
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +43,21 @@ public class LoginActivity extends AppCompatActivity {
     public void login(View view) {
         String username = loginForms.get(0).getText().toString().trim();
         String password = loginForms.get(1).getText().toString().trim();
+
+        employeeViewModel.getIsLoading().observe(this, aBoolean -> {
+            if (aBoolean != null) {
+                if (aBoolean) {
+                    progressBar.setVisibility(View.VISIBLE);
+                } else {
+                    progressBar.setVisibility(View.GONE);
+                    startActivity(new Intent(LoginActivity.this, SplahScreenActivity.class));
+
+                    finish();
+                }
+            } else {
+                progressBar.setVisibility(View.GONE);
+            }
+        });
 
         if (!username.isEmpty() && !password.isEmpty()) {
             employeeViewModel.login(username, password);
