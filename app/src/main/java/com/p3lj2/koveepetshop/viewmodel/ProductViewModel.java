760d15682cfea66fd7a32d2e27b48f9b105a72c9
Natan Hari Pamungkas <natanhp@protonmail.com>
2020-03-11 18:33:24 +0700

    Add a view model for product
    Signed-off-by: Natan Hari Pamungkas <natanhp@protonmail.com>

---
+++ app/src/main/java/com/p3lj2/koveepetshop/viewmodel/ProductViewModel.java
@@ -0,0 +1,38 @@
+package com.p3lj2.koveepetshop.viewmodel;
+
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.p3lj2.koveepetshop.model.EmployeeDataModel;
+import com.p3lj2.koveepetshop.model.ProductResponseModel;
+import com.p3lj2.koveepetshop.repository.EmployeeRepository;
+import com.p3lj2.koveepetshop.repository.ProductRepository;
+
+public class ProductViewModel extends AndroidViewModel {
+    private ProductRepository repository;
+    private EmployeeRepository employeeRepository;
+
+    public ProductViewModel(Application application) {
+        super(application);
+        this.repository = new ProductRepository();
+        this.employeeRepository = new EmployeeRepository(application);
+    }
+
+    public void insert(String token, ProductResponseModel productResponseModel) {
+        repository.insert(token, productResponseModel);
+    }
+
+    public LiveData<EmployeeDataModel> getEmployee() {
+        return employeeRepository.getEmployee();
+    }
+
+    public LiveData<Boolean> getEmployeeLoading() {
+        return employeeRepository.getIsLoading();
+    }
+
+    public LiveData<Boolean> getIsLoading() {
+        return repository.getIsLoading();
+    }
+}
