070c081837daa184ee67bd2957ac77dc404057e8
Natan Hari Pamungkas <natanhp@protonmail.com>
2020-03-13 00:49:05 +0700

    Add updatedby attribute
    Signed-off-by: Natan Hari Pamungkas <natanhp@protonmail.com>

--- app/src/main/java/com/p3lj2/koveepetshop/model/ProductModel.java
+++ app/src/main/java/com/p3lj2/koveepetshop/model/ProductModel.java
@@ -1,11 +1,14 @@
 package com.p3lj2.koveepetshop.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.gson.annotations.Expose;
 
 import lombok.Data;
 
 @Data
-public class ProductModel {
+public class ProductModel implements Parcelable {
     @Expose
     private int id;
 
@@ -25,5 +28,51 @@ public class ProductModel {
     private int createdBy;
 
     @Expose
+    private int updatedBy;
+
+    @Expose
     private int minimumQty;
+
+    public ProductModel() {
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(this.id);
+        dest.writeString(this.productName);
+        dest.writeInt(this.productQuantity);
+        dest.writeDouble(this.productPrice);
+        dest.writeString(this.meassurement);
+        dest.writeInt(this.createdBy);
+        dest.writeInt(this.updatedBy);
+        dest.writeInt(this.minimumQty);
+    }
+
+    protected ProductModel(Parcel in) {
+        this.id = in.readInt();
+        this.productName = in.readString();
+        this.productQuantity = in.readInt();
+        this.productPrice = in.readDouble();
+        this.meassurement = in.readString();
+        this.createdBy = in.readInt();
+        this.updatedBy = in.readInt();
+        this.minimumQty = in.readInt();
+    }
+
+    public static final Parcelable.Creator<ProductModel> CREATOR = new Parcelable.Creator<ProductModel>() {
+        @Override
+        public ProductModel createFromParcel(Parcel source) {
+            return new ProductModel(source);
+        }
+
+        @Override
+        public ProductModel[] newArray(int size) {
+            return new ProductModel[size];
+        }
+    };
 }
