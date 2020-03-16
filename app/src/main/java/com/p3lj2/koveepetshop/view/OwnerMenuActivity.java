ba7a6f7ccc09499d10d7de8475474039891e20e3
Natan Hari Pamungkas <natanhp@protonmail.com>
2020-03-16 21:38:21 +0700

    Replace the owner activity without fragment
    Signed-off-by: Natan Hari Pamungkas <natanhp@protonmail.com>

---
+++ app/src/main/java/com/p3lj2/koveepetshop/view/OwnerMenuActivity.java
@@ -0,0 +1,28 @@
+package com.p3lj2.koveepetshop.view;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.p3lj2.koveepetshop.R;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class OwnerMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_owner_menu);
+
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn_product)
+    public void productOnClick(View view) {
+        startActivity(new Intent(this, ProductActivity.class));
+    }
+}
