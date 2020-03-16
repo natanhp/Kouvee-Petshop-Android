fb5e3b4339197299df38fb8f748a093673404f6e
Natan Hari Pamungkas <natanhp@protonmail.com>
2020-03-16 23:22:17 +0700

    Create a repository for per type
    Signed-off-by: Natan Hari Pamungkas <natanhp@protonmail.com>

---
+++ app/src/main/java/com/p3lj2/koveepetshop/repository/PetTypeRepository.java
@@ -0,0 +1,53 @@
+package com.p3lj2.koveepetshop.repository;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.p3lj2.koveepetshop.endpoint.PetTypeEndpoint;
+import com.p3lj2.koveepetshop.model.PetTypeModel;
+import com.p3lj2.koveepetshop.model.PetTypeSchema;
+import com.p3lj2.koveepetshop.util.RetrofitInstance;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class PetTypeRepository {
+    private PetTypeEndpoint petTypeEndpoint;
+    private static MutableLiveData<Boolean> isLoading = new MutableLiveData<>();
+    private static MutableLiveData<List<PetTypeModel>> petTypeModels = new MutableLiveData<>();
+
+    public PetTypeRepository() {
+        RetrofitInstance retrofitInstance = RetrofitInstance.getRetrofitInstance();
+        petTypeEndpoint = retrofitInstance.getRetrofit().create(PetTypeEndpoint.class);
+    }
+
+    public LiveData<List<PetTypeModel>> getAll(String bearerToken) {
+        isLoading.setValue(true);
+        petTypeEndpoint.getAll("Bearer " + bearerToken).enqueue(new Callback<PetTypeSchema>() {
+            @Override
+            public void onResponse(@NotNull Call<PetTypeSchema> call, @NotNull Response<PetTypeSchema> response) {
+                if (response.body() != null && response.code() == 200) {
+                    petTypeModels.postValue(response.body().getPetTypeModels());
+                }
+
+                isLoading.postValue(false);
+            }
+
+            @Override
+            public void onFailure(@NotNull Call<PetTypeSchema> call, @NotNull Throwable t) {
+                isLoading.postValue(false);
+            }
+        });
+
+        return petTypeModels;
+    }
+
+    public LiveData<Boolean> getIsLoading() {
+        return isLoading;
+    }
+}
