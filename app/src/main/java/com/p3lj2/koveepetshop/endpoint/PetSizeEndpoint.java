c7d8cc7cc41c564ced14243a872851830316aa9d
Natan Hari Pamungkas <natanhp@protonmail.com>
2020-03-18 01:19:31 +0700

    Add insert pet size endpoint
    Signed-off-by: Natan Hari Pamungkas <natanhp@protonmail.com>

--- app/src/main/java/com/p3lj2/koveepetshop/endpoint/PetSizeEndpoint.java
+++ app/src/main/java/com/p3lj2/koveepetshop/endpoint/PetSizeEndpoint.java
@@ -1,12 +1,18 @@
 package com.p3lj2.koveepetshop.endpoint;
 
+import com.p3lj2.koveepetshop.model.PetSizeModel;
 import com.p3lj2.koveepetshop.model.PetSizeSchema;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.POST;
 
 public interface PetSizeEndpoint {
     @GET("petsizes/getall")
     Call<PetSizeSchema> getAll(@Header("Authorization") String bearerToken);
+
+    @POST("petsizes/insert")
+    Call<PetSizeSchema> insert(@Header("Authorization") String bearerToken, @Body PetSizeModel petSizeModel);
 }
