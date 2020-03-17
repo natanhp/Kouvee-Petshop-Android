e68d791a3efb2354ed76c75fff983f81b873ab05
Natan Hari Pamungkas <natanhp@protonmail.com>
2020-03-17 19:27:12 +0700

    Make the model parcelable
    Signed-off-by: Natan Hari Pamungkas <natanhp@protonmail.com>

--- app/src/main/java/com/p3lj2/koveepetshop/model/PetTypeModel.java
+++ app/src/main/java/com/p3lj2/koveepetshop/model/PetTypeModel.java
@@ -1,11 +1,14 @@
 package com.p3lj2.koveepetshop.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.gson.annotations.Expose;
 
 import lombok.Data;
 
 @Data
-public class PetTypeModel {
+public class PetTypeModel implements Parcelable {
 
     @Expose
     private int id;
@@ -21,4 +24,41 @@ public class PetTypeModel {
 
     @Expose
     private int deletedBy;
+
+    public PetTypeModel() {
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(this.id);
+        dest.writeString(this.type);
+        dest.writeInt(this.createdBy);
+        dest.writeInt(this.updatedBy);
+        dest.writeInt(this.deletedBy);
+    }
+
+    protected PetTypeModel(Parcel in) {
+        this.id = in.readInt();
+        this.type = in.readString();
+        this.createdBy = in.readInt();
+        this.updatedBy = in.readInt();
+        this.deletedBy = in.readInt();
+    }
+
+    public static final Parcelable.Creator<PetTypeModel> CREATOR = new Parcelable.Creator<PetTypeModel>() {
+        @Override
+        public PetTypeModel createFromParcel(Parcel source) {
+            return new PetTypeModel(source);
+        }
+
+        @Override
+        public PetTypeModel[] newArray(int size) {
+            return new PetTypeModel[size];
+        }
+    };
 }
