a04ce6e189b21a2dd1d82a0a126d8ae27be59d24
Natan Hari Pamungkas <natanhp@protonmail.com>
2020-03-17 23:18:09 +0700

    Create models for pet size
    Signed-off-by: Natan Hari Pamungkas <natanhp@protonmail.com>

---
+++ app/src/main/java/com/p3lj2/koveepetshop/model/PetSizeModel.java
@@ -0,0 +1,61 @@
+package com.p3lj2.koveepetshop.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.google.gson.annotations.Expose;
+
+import lombok.Data;
+
+@Data
+public class PetSizeModel implements Parcelable {
+
+    @Expose
+    private int id;
+
+    @Expose
+    private String size;
+
+    @Expose
+    private int createdBy;
+
+    @Expose
+    private int updatedBy;
+
+    @Expose
+    private int deletedBy;
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(this.id);
+        dest.writeString(this.size);
+        dest.writeInt(this.createdBy);
+        dest.writeInt(this.updatedBy);
+        dest.writeInt(this.deletedBy);
+    }
+
+    protected PetSizeModel(Parcel in) {
+        this.id = in.readInt();
+        this.size = in.readString();
+        this.createdBy = in.readInt();
+        this.updatedBy = in.readInt();
+        this.deletedBy = in.readInt();
+    }
+
+    public static final Parcelable.Creator<PetSizeModel> CREATOR = new Parcelable.Creator<PetSizeModel>() {
+        @Override
+        public PetSizeModel createFromParcel(Parcel source) {
+            return new PetSizeModel(source);
+        }
+
+        @Override
+        public PetSizeModel[] newArray(int size) {
+            return new PetSizeModel[size];
+        }
+    };
+}
