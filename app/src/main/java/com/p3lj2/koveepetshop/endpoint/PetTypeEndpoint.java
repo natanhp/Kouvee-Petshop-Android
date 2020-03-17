807ae300135f802f538e9be95eb52ff6621e667a
Natan Hari Pamungkas <natanhp@protonmail.com>
2020-03-17 19:57:35 +0700

    Add delete endpoint
    Signed-off-by: Natan Hari Pamungkas <natanhp@protonmail.com>

--- app/src/main/java/com/p3lj2/koveepetshop/endpoint/PetTypeEndpoint.java
+++ app/src/main/java/com/p3lj2/koveepetshop/endpoint/PetTypeEndpoint.java
@@ -5,10 +5,12 @@ import com.p3lj2.koveepetshop.model.PetTypeSchema;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
+import retrofit2.http.Path;
 
 public interface PetTypeEndpoint {
     @GET("pettypes/getall")
@@ -19,4 +21,7 @@ public interface PetTypeEndpoint {
 
     @PUT("pettypes/update")
     Call<PetTypeSchema> update(@Header("Authorization") String bearerToken, @Body PetTypeModel petTypeModel);
+
+    @DELETE("pettypes/delete/{id}/{ownerId}")
+    Call<PetTypeSchema> delete(@Header("Authorization") String bearerToken, @Path("id") int petTypeId, @Path("ownerId") int ownerId);
 }
