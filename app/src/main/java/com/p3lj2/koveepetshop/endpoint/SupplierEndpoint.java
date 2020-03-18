200fccdd231cebf1ce7d86b5d379c4c8c5f8a5aa
Natan Hari Pamungkas <natanhp@protonmail.com>
2020-03-18 12:27:49 +0700

    Add insert supplier endpoint
    Signed-off-by: Natan Hari Pamungkas <natanhp@protonmail.com>

--- app/src/main/java/com/p3lj2/koveepetshop/endpoint/SupplierEndpoint.java
+++ app/src/main/java/com/p3lj2/koveepetshop/endpoint/SupplierEndpoint.java
@@ -1,12 +1,18 @@
 package com.p3lj2.koveepetshop.endpoint;
 
+import com.p3lj2.koveepetshop.model.SupplierModel;
 import com.p3lj2.koveepetshop.model.SupplierSchema;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.POST;
 
 public interface SupplierEndpoint {
     @GET("suppliers/getall")
     Call<SupplierSchema> getAll(@Header("Authorization") String bearerToken);
+
+    @POST("suppliers/insert")
+    Call<SupplierSchema> insert(@Header("Authorization") String bearerToken, @Body SupplierModel supplierModel);
 }
