d969f23e4ee042311d8510f9077aea71bd596cf7
Natan Hari Pamungkas <natanhp@protonmail.com>
2020-03-18 21:23:01 +0700

    Add insert service endpoint
    Signed-off-by: Natan Hari Pamungkas <natanhp@protonmail.com>

--- app/src/main/java/com/p3lj2/koveepetshop/endpoint/ServiceEndpoint.java
+++ app/src/main/java/com/p3lj2/koveepetshop/endpoint/ServiceEndpoint.java
@@ -1,12 +1,18 @@
 package com.p3lj2.koveepetshop.endpoint;
 
+import com.p3lj2.koveepetshop.model.ServiceModel;
 import com.p3lj2.koveepetshop.model.ServiceSchema;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.POST;
 
 public interface ServiceEndpoint {
     @GET("services/getall")
     Call<ServiceSchema> getAll(@Header("Authorization") String bearerToken);
+
+    @POST("services/insert")
+    Call<ServiceSchema> insert(@Header("Authorization") String bearerToken, @Body ServiceModel serviceModel);
 }
