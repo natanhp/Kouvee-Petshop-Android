fcb1d612c91d3604fd2f09be0a709ad429132d1b
Natan Hari Pamungkas <natanhp@protonmail.com>
2020-03-18 21:06:20 +0700

    Create repository for service
    Signed-off-by: Natan Hari Pamungkas <natanhp@protonmail.com>

---
+++ app/src/main/java/com/p3lj2/koveepetshop/repository/ServiceRepository.java
@@ -0,0 +1,53 @@
+package com.p3lj2.koveepetshop.repository;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.p3lj2.koveepetshop.endpoint.ServiceEndpoint;
+import com.p3lj2.koveepetshop.model.ServiceModel;
+import com.p3lj2.koveepetshop.model.ServiceSchema;
+import com.p3lj2.koveepetshop.util.RetrofitInstance;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class ServiceRepository {
+    private ServiceEndpoint serviceEndpoint;
+    private MutableLiveData<Boolean> isLoading = new MutableLiveData<>();
+    private MutableLiveData<List<ServiceModel>> serviceModels = new MutableLiveData<>();
+
+    public ServiceRepository() {
+        RetrofitInstance retrofitInstance = RetrofitInstance.getRetrofitInstance();
+        serviceEndpoint = retrofitInstance.getRetrofit().create(ServiceEndpoint.class);
+    }
+
+    public LiveData<List<ServiceModel>> getAll(String bearerToken) {
+        isLoading.setValue(true);
+        serviceEndpoint.getAll("Bearer " + bearerToken).enqueue(new Callback<ServiceSchema>() {
+            @Override
+            public void onResponse(@NotNull Call<ServiceSchema> call, @NotNull Response<ServiceSchema> response) {
+                if (response.body() != null && response.code() == 200) {
+                    serviceModels.postValue(response.body().getServiceModels());
+                }
+
+                isLoading.postValue(false);
+            }
+
+            @Override
+            public void onFailure(@NotNull Call<ServiceSchema> call, @NotNull Throwable t) {
+                isLoading.postValue(false);
+            }
+        });
+
+        return serviceModels;
+    }
+
+    public LiveData<Boolean> getIsLoading() {
+        return isLoading;
+    }
+}
