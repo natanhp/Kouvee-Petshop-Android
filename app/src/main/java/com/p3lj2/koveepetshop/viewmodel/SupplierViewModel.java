3f9ee63b01c6a90013d2b24762ad513cfa9f3522
Natan Hari Pamungkas <natanhp@protonmail.com>
2020-03-18 10:51:49 +0700

    Add supplier view model
    Signed-off-by: Natan Hari Pamungkas <natanhp@protonmail.com>

---
+++ app/src/main/java/com/p3lj2/koveepetshop/viewmodel/SupplierViewModel.java
@@ -0,0 +1,42 @@
+package com.p3lj2.koveepetshop.viewmodel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.p3lj2.koveepetshop.model.EmployeeDataModel;
+import com.p3lj2.koveepetshop.model.SupplierModel;
+import com.p3lj2.koveepetshop.repository.EmployeeRepository;
+import com.p3lj2.koveepetshop.repository.SupplierRepository;
+
+import java.util.List;
+
+public class SupplierViewModel extends AndroidViewModel {
+    private SupplierRepository supplierRepository;
+    private EmployeeRepository employeeRepository;
+
+    public SupplierViewModel(@NonNull Application application) {
+        super(application);
+
+        supplierRepository = new SupplierRepository();
+        employeeRepository = new EmployeeRepository(application);
+    }
+
+    public LiveData<List<SupplierModel>> getAll(String bearerToken) {
+        return supplierRepository.getAll(bearerToken);
+    }
+
+    public LiveData<Boolean> getIsLoading() {
+        return supplierRepository.getIsLoading();
+    }
+
+    public LiveData<EmployeeDataModel> getEmployee() {
+        return employeeRepository.getEmployee();
+    }
+
+    public LiveData<Boolean> getEmployeeIsLoading() {
+        return employeeRepository.getIsLoading();
+    }
+}
