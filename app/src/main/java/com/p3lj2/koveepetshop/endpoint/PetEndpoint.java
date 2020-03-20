bf593765a36d0eaf88d52ce7a0ac01c731fd092f
Natan Hari Pamungkas <natanhp@protonmail.com>
2020-03-20 21:12:52 +0700

    Add insert endpoint for pet
    Signed-off-by: Natan Hari Pamungkas <natanhp@protonmail.com>

--- app/src/main/java/com/p3lj2/koveepetshop/endpoint/PetEndpoint.java
+++ app/src/main/java/com/p3lj2/koveepetshop/endpoint/PetEndpoint.java
@@ -1,13 +1,19 @@
 package com.p3lj2.koveepetshop.endpoint;
 
+import com.p3lj2.koveepetshop.model.PetModel;
 import com.p3lj2.koveepetshop.model.PetSchema;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.POST;
 
 public interface PetEndpoint {
 
     @GET("pets/getall")
     Call<PetSchema> getAll(@Header("Authorization") String bearerToken);
+
+    @POST("pets/insert")
+    Call<PetSchema> insert(@Header("Authorization") String bearerToken, @Body PetModel petModel);
 }
