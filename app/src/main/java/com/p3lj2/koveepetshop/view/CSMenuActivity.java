269993fcc0650947afbbc5b4244c4da584faf1bc
Natan Hari Pamungkas <natanhp@protonmail.com>
2020-03-21 13:33:37 +0700

    Delete the unused menu and add log out function
    Signed-off-by: Natan Hari Pamungkas <natanhp@protonmail.com>

--- app/src/main/java/com/p3lj2/koveepetshop/view/CSMenuActivity.java
+++ app/src/main/java/com/p3lj2/koveepetshop/view/CSMenuActivity.java
@@ -5,13 +5,17 @@ import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.p3lj2.koveepetshop.R;
+import com.p3lj2.koveepetshop.util.Util;
+import com.p3lj2.koveepetshop.viewmodel.EmployeeViewModel;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class CSMenuActivity extends AppCompatActivity {
+    private EmployeeViewModel employeeViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,8 @@ public class CSMenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_c_s_menu);
 
         ButterKnife.bind(this);
+
+        employeeViewModel = new ViewModelProvider.AndroidViewModelFactory(getApplication()).create(EmployeeViewModel.class);
     }
 
     @OnClick(R.id.btn_pet)
@@ -29,5 +35,19 @@ public class CSMenuActivity extends AppCompatActivity {
     @OnClick(R.id.btn_customer)
     public void onClickCustomer(View view) {
         startActivity(new Intent(this, CustomersActivity.class));
+    }
+
+    @OnClick(R.id.btn_logout)
+    public void logoutOnClick(View view) {
+        Util.confirmationDialog(getString(R.string.logout), getString(R.string.logout_confirmation),this)
+                .setPositiveButton(getString(R.string.yes), (dialogInterface, i) -> {
+                    employeeViewModel.getEmployee().observe(this, employeeDataModel -> {
+                        employeeViewModel.delete(employeeDataModel);
+                        startActivity(new Intent(CSMenuActivity.this, LoginActivity.class));
+                        finish();
+                    });
+                })
+                .setNegativeButton(getString(R.string.no), null)
+                .show();
     }
 }
