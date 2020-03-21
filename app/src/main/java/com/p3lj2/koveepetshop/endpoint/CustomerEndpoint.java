b1299e6dccf21657ecb4e23885ec24d73f9e5d6a
Natan Hari Pamungkas <natanhp@protonmail.com>
2020-03-21 12:45:07 +0700

    Add insert customer endpoint
    Signed-off-by: Natan Hari Pamungkas <natanhp@protonmail.com>

--- app/src/main/java/com/p3lj2/koveepetshop/endpoint/CustomerEndpoint.java
+++ app/src/main/java/com/p3lj2/koveepetshop/endpoint/CustomerEndpoint.java
@@ -1,13 +1,19 @@
 package com.p3lj2.koveepetshop.endpoint;
 
+import com.p3lj2.koveepetshop.model.CustomerModel;
 import com.p3lj2.koveepetshop.model.CustomerSchema;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.POST;
 
 public interface CustomerEndpoint {
 
     @GET("customers/getall")
     Call<CustomerSchema> getAll(@Header("Authorization") String bearerToken);
+
+    @POST("customers/insert")
+    Call<CustomerSchema> insert(@Header("Authorization") String bearerToken, @Body CustomerModel customerModel);
 }
