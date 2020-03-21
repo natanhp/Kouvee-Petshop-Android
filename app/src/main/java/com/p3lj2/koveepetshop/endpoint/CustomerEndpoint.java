12c128dd22e5e777f2bdf48a0c9ee6921d4dd613
Natan Hari Pamungkas <natanhp@protonmail.com>
2020-03-21 13:13:49 +0700

    Add delete customer endpoint
    Signed-off-by: Natan Hari Pamungkas <natanhp@protonmail.com>

--- app/src/main/java/com/p3lj2/koveepetshop/endpoint/CustomerEndpoint.java
+++ app/src/main/java/com/p3lj2/koveepetshop/endpoint/CustomerEndpoint.java
@@ -5,10 +5,12 @@ import com.p3lj2.koveepetshop.model.CustomerSchema;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
+import retrofit2.http.Path;
 
 public interface CustomerEndpoint {
 
@@ -20,4 +22,7 @@ public interface CustomerEndpoint {
 
     @PUT("customers/update")
     Call<CustomerSchema> update(@Header("Authorization") String bearerToken, @Body CustomerModel customerModel);
+
+    @DELETE("customers/delete/{id}/{csId}")
+    Call<CustomerSchema> delete(@Header("Authorization") String bearerToken, @Path("id") int customerId, @Path("csId") int csId);
 }
