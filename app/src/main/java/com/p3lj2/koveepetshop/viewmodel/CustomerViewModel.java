214960da8b39ff3831a629d5847e2aa934e6b2e0
Natan Hari Pamungkas <natanhp@protonmail.com>
2020-03-21 12:08:29 +0700

    Add view model for customer
    Signed-off-by: Natan Hari Pamungkas <natanhp@protonmail.com>

---
+++ app/src/main/java/com/p3lj2/koveepetshop/viewmodel/CustomerViewModel.java
@@ -0,0 +1,46 @@
+package com.p3lj2.koveepetshop.viewmodel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.p3lj2.koveepetshop.model.CustomerModel;
+import com.p3lj2.koveepetshop.model.EmployeeDataModel;
+import com.p3lj2.koveepetshop.repository.CustomerRepository;
+import com.p3lj2.koveepetshop.repository.EmployeeRepository;
+
+import java.util.List;
+
+public class CustomerViewModel extends AndroidViewModel {
+    private CustomerRepository customerRepository;
+    private EmployeeRepository employeeRepository;
+
+    public CustomerViewModel(@NonNull Application application) {
+        super(application);
+
+        customerRepository = new CustomerRepository();
+        employeeRepository = new EmployeeRepository(application);
+    }
+
+    public LiveData<List<CustomerModel>> getAll(String bearerToken) {
+        return customerRepository.getAll(bearerToken);
+    }
+
+    public void insert(String bearerToken, CustomerModel customerModel) {
+        customerRepository.insert(bearerToken, customerModel);
+    }
+
+    public LiveData<Boolean> getIsLoading() {
+        return customerRepository.getIsLoading();
+    }
+
+    public LiveData<EmployeeDataModel> getEmployee() {
+        return employeeRepository.getEmployee();
+    }
+
+    public LiveData<Boolean> getEmployeeIsLoading() {
+        return employeeRepository.getIsLoading();
+    }
+}
