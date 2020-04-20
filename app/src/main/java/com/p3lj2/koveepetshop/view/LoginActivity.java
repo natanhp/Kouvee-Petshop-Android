c682eb60c275ca4c0b4c79e1b258ba8b77ae4dde
Natan Hari Pamungkas <natanhp@protonmail.com>
2020-04-21 05:39:28 +0700

    Add function for creating a notification channel
    Signed-off-by: Natan Hari Pamungkas <natanhp@protonmail.com>

--- app/src/main/java/com/p3lj2/koveepetshop/view/LoginActivity.java
+++ app/src/main/java/com/p3lj2/koveepetshop/view/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.p3lj2.koveepetshop.view;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -73,6 +76,7 @@ public class LoginActivity extends AppCompatActivity {
                     if (employeeDataModel.getRole().equalsIgnoreCase("owner")) {
                         startActivity(new Intent(LoginActivity.this, OwnerMenuActivity.class));
                         saveFCMToken(fcmToken, employeeDataModel);
+                        createNotificationChannel();
                         finish();
                     } else if (employeeDataModel.getRole().equalsIgnoreCase("cs")) {
                         startActivity(new Intent(LoginActivity.this, CSMenuActivity.class));
@@ -113,6 +117,20 @@ public class LoginActivity extends AppCompatActivity {
             progressBar.setVisibility(View.VISIBLE);
         } else {
             progressBar.setVisibility(View.GONE);
+        }
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT > -Build.VERSION_CODES.O) {
+            CharSequence name = "Min Qty Warning";
+            String description = "Channel for min qty warning";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("min_qty_warning", name, importance);
+            channel.setDescription(description);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            if (notificationManager != null) {
+                notificationManager.createNotificationChannel(channel);
+            }
         }
     }
 }
