d3d49809336b7965932f3e15e19103eefc7ade44
Natan Hari Pamungkas <natanhp@protonmail.com>
2020-04-27 14:10:19 +0700

    Add activity and fragment for product restock
    Signed-off-by: Natan Hari Pamungkas <natanhp@protonmail.com>

--- app/src/main/java/com/p3lj2/koveepetshop/view/restock/RestockActivity.java
+++ app/src/main/java/com/p3lj2/koveepetshop/view/restock/RestockActivity.java
@@ -21,7 +21,7 @@ public class RestockActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.product_stock, R.id.navigation_product_restock_detail, R.id.navigation_product_arrived)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
