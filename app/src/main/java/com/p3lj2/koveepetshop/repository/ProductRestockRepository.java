69470a934d03c7fdd6ea805fda8ba9e0e0e9b147
Natan Hari Pamungkas <natanhp@protonmail.com>
2020-04-27 14:09:32 +0700

    Create getAll and confirm function
    Signed-off-by: Natan Hari Pamungkas <natanhp@protonmail.com>

--- app/src/main/java/com/p3lj2/koveepetshop/repository/ProductRestockRepository.java
+++ app/src/main/java/com/p3lj2/koveepetshop/repository/ProductRestockRepository.java
@@ -4,7 +4,6 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
 import com.p3lj2.koveepetshop.endpoint.ProductRestockEndpoint;
-import com.p3lj2.koveepetshop.model.ProductRestockDetail;
 import com.p3lj2.koveepetshop.model.ProductRestockModel;
 import com.p3lj2.koveepetshop.model.ResponseSchema;
 import com.p3lj2.koveepetshop.util.RetrofitInstance;
@@ -20,7 +19,7 @@ import retrofit2.Response;
 public class ProductRestockRepository {
     private ProductRestockEndpoint productRestockEndpoint;
     private static MutableLiveData<Boolean> isLoading = new MutableLiveData<>();
-    private static MutableLiveData<List<ProductRestockDetail>> restockModels = new MutableLiveData<>();
+    private static MutableLiveData<List<ProductRestockModel>> restockModels = new MutableLiveData<>();
 
     public ProductRestockRepository() {
         RetrofitInstance retrofitInstance = RetrofitInstance.getRetrofitInstance();
@@ -42,7 +41,43 @@ public class ProductRestockRepository {
         });
     }
 
+    public LiveData<List<ProductRestockModel>> getAll(String bearerToken) {
+        isLoading.setValue(true);
+        productRestockEndpoint.getAll("Bearer " + bearerToken).enqueue(new Callback<ResponseSchema<ProductRestockModel>>() {
+            @Override
+            public void onResponse(@NotNull Call<ResponseSchema<ProductRestockModel>> call, @NotNull Response<ResponseSchema<ProductRestockModel>> response) {
+                if (response.body() != null && response.code() == 200) {
+                    restockModels.postValue(response.body().getData());
+                }
+
+                isLoading.postValue(false);
+            }
+
+            @Override
+            public void onFailure(@NotNull Call<ResponseSchema<ProductRestockModel>> call, @NotNull Throwable t) {
+                isLoading.postValue(false);
+            }
+        });
+
+        return restockModels;
+    }
+
     public LiveData<Boolean> getIsLoading() {
         return isLoading;
+    }
+
+    public void confirm(String bearerToken, String productId, int ownerId) {
+        isLoading.setValue(true);
+        productRestockEndpoint.confirm("Bearer " + bearerToken, productId, ownerId).enqueue(new Callback<ResponseSchema<ProductRestockModel>>() {
+            @Override
+            public void onResponse(Call<ResponseSchema<ProductRestockModel>> call, Response<ResponseSchema<ProductRestockModel>> response) {
+                isLoading.postValue(false);
+            }
+
+            @Override
+            public void onFailure(Call<ResponseSchema<ProductRestockModel>> call, Throwable t) {
+                isLoading.postValue(false);
+            }
+        });
     }
 }
