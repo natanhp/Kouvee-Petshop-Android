6f871dd57317b14d54ff0d9f361acbafac66d4dd
Natan Hari Pamungkas <natanhp@protonmail.com>
2020-05-16 08:15:10 +0700

    Add updated by attribute and make the class parcelable
    Signed-off-by: Natan Hari Pamungkas <natanhp@protonmail.com>

--- app/src/main/java/com/p3lj2/koveepetshop/model/ProductTransactionDetailModel.java
+++ app/src/main/java/com/p3lj2/koveepetshop/model/ProductTransactionDetailModel.java
@@ -1,12 +1,15 @@
 package com.p3lj2.koveepetshop.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 import lombok.Data;
 
 @Data
-public class ProductTransactionDetailModel {
+public class ProductTransactionDetailModel implements Parcelable {
     @Expose
     private int id;
 
@@ -14,9 +17,56 @@ public class ProductTransactionDetailModel {
     private int createdBy;
 
     @Expose
+    private int updatedBy;
+
+    @Expose
     private int itemQty;
 
     @Expose
     @SerializedName("Products_id")
     private int productId;
+
+    @Expose(serialize = false)
+    @SerializedName("product")
+    private ProductModel productModel;
+
+    public ProductTransactionDetailModel() {
+    }
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(this.id);
+        dest.writeInt(this.createdBy);
+        dest.writeInt(this.updatedBy);
+        dest.writeInt(this.itemQty);
+        dest.writeInt(this.productId);
+        dest.writeParcelable(this.productModel, flags);
+    }
+
+    protected ProductTransactionDetailModel(Parcel in) {
+        this.id = in.readInt();
+        this.createdBy = in.readInt();
+        this.updatedBy = in.readInt();
+        this.itemQty = in.readInt();
+        this.productId = in.readInt();
+        this.productModel = in.readParcelable(ProductModel.class.getClassLoader());
+    }
+
+    public static final Creator<ProductTransactionDetailModel> CREATOR = new Creator<ProductTransactionDetailModel>() {
+        @Override
+        public ProductTransactionDetailModel createFromParcel(Parcel source) {
+            return new ProductTransactionDetailModel(source);
+        }
+
+        @Override
+        public ProductTransactionDetailModel[] newArray(int size) {
+            return new ProductTransactionDetailModel[size];
+        }
+    };
 }
