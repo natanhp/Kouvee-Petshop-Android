397b46d621dc0e220442d303afd23e34c206b1b7
Natan Hari Pamungkas <natanhp@protonmail.com>
2020-05-21 01:24:10 +0700

    Create options menu for logout
    Signed-off-by: Natan Hari Pamungkas <natanhp@protonmail.com>

--- app/src/main/java/com/p3lj2/koveepetshop/view/cashier/payment/PaymentActivity.java
+++ app/src/main/java/com/p3lj2/koveepetshop/view/cashier/payment/PaymentActivity.java
@@ -1,8 +1,14 @@
 package com.p3lj2.koveepetshop.view.cashier.payment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -10,9 +16,13 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.p3lj2.koveepetshop.R;
+import com.p3lj2.koveepetshop.util.Util;
+import com.p3lj2.koveepetshop.view.LoginActivity;
+import com.p3lj2.koveepetshop.viewmodel.EmployeeViewModel;
 
 public class PaymentActivity extends AppCompatActivity {
-    private  AppBarConfiguration appBarConfiguration;
+    private AppBarConfiguration appBarConfiguration;
+    private EmployeeViewModel employeeViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +37,8 @@ public class PaymentActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+        employeeViewModel = new ViewModelProvider.AndroidViewModelFactory(getApplication()).create(EmployeeViewModel.class);
     }
 
     @Override
@@ -34,5 +46,31 @@ public class PaymentActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.logout_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.logout) {
+            Util.confirmationDialog(getString(R.string.logout), getString(R.string.logout_confirmation), this)
+                    .setPositiveButton(getString(R.string.yes), (dialogInterface, i) -> employeeViewModel.getEmployee().observe(this, employeeDataModel -> {
+                        employeeViewModel.delete(employeeDataModel);
+                        startActivity(new Intent(PaymentActivity.this, LoginActivity.class));
+                        finish();
+                    }))
+                    .setNegativeButton(getString(R.string.no), null)
+                    .show();
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
