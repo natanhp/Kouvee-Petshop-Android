22c868ce7f4dcb90721aa6456655541302ced967
Natan Hari Pamungkas <natanhp@protonmail.com>
2020-05-21 02:25:01 +0700

    Apply sorting to product list for guest
    Signed-off-by: Natan Hari Pamungkas <natanhp@protonmail.com>

--- app/src/main/java/com/p3lj2/koveepetshop/view/product/guest/ProductListActivity.java
+++ app/src/main/java/com/p3lj2/koveepetshop/view/product/guest/ProductListActivity.java
@@ -1,9 +1,17 @@
 package com.p3lj2.koveepetshop.view.product.guest;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -11,7 +19,12 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.p3lj2.koveepetshop.R;
 import com.p3lj2.koveepetshop.adapter.ProductAdapter;
+import com.p3lj2.koveepetshop.model.ProductResponseModel;
+import com.p3lj2.koveepetshop.util.Util;
 import com.p3lj2.koveepetshop.viewmodel.ProductViewModel;
+
+import java.util.Collections;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -25,6 +38,7 @@ public class ProductListActivity extends AppCompatActivity {
 
     private ProductAdapter productAdapter;
     private ProductViewModel productViewModel;
+    private static int checkedButton = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,5 +75,90 @@ public class ProductListActivity extends AppCompatActivity {
         } else {
             progressBar.setVisibility(View.GONE);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.product_list_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.sort) {
+            sortingDialog();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void sortingDialog() {
+        View view = LayoutInflater.from(this).inflate(R.layout.sorting_layout, this.findViewById(android.R.id.content), false);
+        RadioGroup rgSort = view.findViewById(R.id.rg_sort);
+        if (checkedButton == 0) {
+            checkedButton = R.id.rb_lower_price;
+        }
+
+        RadioButton radioButton = view.findViewById(checkedButton);
+        radioButton.setChecked(true);
+
+        rgSort.setOnCheckedChangeListener((radioGroup, i) -> checkedButton = i);
+        Util.confirmationDialog(getString(R.string.sort_product), "", this)
+                .setView(view)
+                .setPositiveButton(R.string.yes, (dialogInterface, i) -> {
+                    switch (checkedButton) {
+                        case R.id.rb_lower_price:
+                            sortBy("asc_price");
+                            break;
+                        case R.id.rb_higher_price:
+                            sortBy("dsc_price");
+                            break;
+                        case R.id.rb_least_stock:
+                            sortBy("asc_stock");
+                            break;
+                        case R.id.rb_more_stock:
+                            sortBy("dsc_stock");
+                            break;
+                        default:
+                            Toast.makeText(this, getString(R.string.something_wrong_msg), Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton(R.string.no, (dialogInterface, i) -> dialogInterface.cancel())
+                .show();
+    }
+
+    private void sortBy(String order) {
+        List<ProductResponseModel> productResponseModels = productAdapter.getProductResponseModels();
+
+        Collections.sort(productResponseModels, (productResponseModel, t1) -> {
+            double priceLeft = productResponseModel.getProductModel().getProductPrice();
+            double priceRight = t1.getProductModel().getProductPrice();
+
+            if (order.equals("asc_price")) {
+                return Double.compare(priceLeft, priceRight);
+            } else if (order.equals("dsc_price")) {
+                return Double.compare(priceRight, priceLeft);
+            }
+
+            return 0;
+        });
+
+        Collections.sort(productResponseModels, (productResponseModel, t1) -> {
+            int stockLeft = productResponseModel.getProductModel().getProductQuantity();
+            int stockRight = t1.getProductModel().getProductQuantity();
+
+            if (order.equals("asc_stock")) {
+                return Integer.compare(stockLeft, stockRight);
+            } else if (order.equals("dsc_stock")) {
+                return Integer.compare(stockRight, stockLeft);
+            }
+
+            return 0;
+        });
+
+        productAdapter.setProductResponseModels(productResponseModels);
     }
 }
