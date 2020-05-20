2db0228f87756a60669e41278eb9ce5a6e7e3d0b
Natan Hari Pamungkas <natanhp@protonmail.com>
2020-05-21 01:29:23 +0700

    Refactor the payment activity
    Signed-off-by: Natan Hari Pamungkas <natanhp@protonmail.com>

--- app/src/main/java/com/p3lj2/koveepetshop/view/cashier/payment/PaymentActivity.java
+++ app/src/main/java/com/p3lj2/koveepetshop/view/cashier/payment/PaymentActivity.java
@@ -32,7 +32,7 @@ public class PaymentActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_product_transaction_list, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_product_transaction_list)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
